6f224fea948502efc333a18e5e63784d90aefa04
louispjeldnaes <96472299+louispjeldnaes@users.noreply.github.com>
2023-02-24 11:23:15 +0100

    Kom I gang side tilføjet

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+/*BESTILLING STEP 1*/
+
+let toPers = document.getElementById("toPers");
+let antalPers = document.getElementById("antalPers");
+
+toPers.addEventlistener("click", visAntal);
+
+function visAntal() {
+  alert("Hej med dig ekstern");
+}
